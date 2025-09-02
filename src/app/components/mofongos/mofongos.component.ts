cc90138ec382106b64e1b444f82e6399293c8bec
Joel <joeldiaz703.jd@gmail.com>
2025-09-01 21:06:04 -0400

    fixed pictures

--- src/app/components/mofongos/mofongos.component.ts
+++ src/app/components/mofongos/mofongos.component.ts
@@ -11,7 +11,7 @@ import { HeaderComponent } from "../header/header.component";
 })
 export class MofongosComponent {
 productos:productos[] =[
-  {id:1,nombre:'Mofongo grande',descripcion:'Cerdo',cantidad:0,precio:500,imagen:'/Images/mofongo/mofongo/jpg'},
+  {id:1,nombre:'Mofongo grande',descripcion:'Cerdo',cantidad:0,precio:500,imagen:'/Images/mofongo/mofongo.jpg'},
   {id:2,nombre:'Mofongo pequeño',descripcion:'Camarones',cantidad:0,precio:450,imagen:'/Images/mofongo/mofcamarones.jpg'},
   {id:3,nombre:'Mofongo mediano',descripcion:'Mixto',cantidad:0,precio:550,imagen:'/Images/mofongo/mfngo.jpeg'}
 ]
